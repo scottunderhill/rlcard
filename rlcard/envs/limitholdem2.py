90c3efc2b60c7c9e067e8d454087baffe2ad91e1
Scott Underhill <underhill.scott@gmail.com>
2019-12-23 21:29:49 -0500

    Fixing up arrays

--- rlcard/envs/limitholdem2.py
+++ rlcard/envs/limitholdem2.py
@@ -72,7 +72,9 @@ class LimitholdemEnv2(Env):
         obs = np.zeros(52)
         obs[idx] = 1
         obs.append(handindex)
-        obs.append(np.zeros(20))
+        obs = np.concatenate(obs, handindex)
+        obs = np.concatenate(obs, np.zeros(20))
+        #obs.append()
         for i, num in enumerate(raise_nums):
             obs[52 + 34 + i * 5 + num] = 1
         processed_state['obs'] = obs
